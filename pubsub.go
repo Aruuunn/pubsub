7b10aeaf67a8c2fdf210f7dbb9189d537ecd05af
Arun Murugan <arunmurugan200@gmail.com>
2021-07-12 22:35:27 +0530

    added documentation and format code

--- pubsub.go
+++ pubsub.go
@@ -1,32 +1,36 @@
+//Package pubsub implements a simple Publish Subscribe Pattern in pure golang
 package pubsub
 
 import "context"
 
+// Message is the format in which data is being passed from one channel to another
 type Message struct {
 	ChannelName string
-	Value interface{}
+	Value       interface{}
 }
 
-
+//PubSub is type which encapsulates the data related to the publish-subscriber pattern implementation.
+//It is to be instantiated only using New() function
 type PubSub struct {
-	channels map[string] chan Message
-	context context.Context
+	channels map[string]chan Message
+	context  context.Context
 }
 
+//Subscription will be returned after a subscription. Should be unsubscribed on clean up
 type Subscription struct {
 	pipeChannel <-chan Message
-	cancel context.CancelFunc
+	cancel      context.CancelFunc
 }
 
-
+//The function to instantiate PubSub
 func New(ctx context.Context) *PubSub {
-	return &PubSub {
+	return &PubSub{
 		channels: make(map[string]chan Message),
-		context: ctx,
+		context:  ctx,
 	}
 }
 
-
+//Returns go channel related to the channelName, creates one if not present
 func (p *PubSub) getChannel(channelName string) chan Message {
 	channel, ok := p.channels[channelName]
 
@@ -38,30 +42,31 @@ func (p *PubSub) getChannel(channelName string) chan Message {
 	return channel
 }
 
-
+//Publish data to the specified channels
 func (p *PubSub) Publish(channelNames []string, value interface{}) {
 	for _, channelName := range channelNames {
 		channel := p.getChannel(channelName)
 
-		go func () {
+		go func() {
 
-			message :=  Message {
+			message := Message{
 				ChannelName: channelName,
-				Value: value,
+				Value:       value,
 			}
 
 			for {
 				select {
-					case channel <- message:
-						return
-					case <-p.context.Done():
-						return
- 				}
+				case channel <- message:
+					return
+				case <-p.context.Done():
+					return
+				}
 			}
 		}()
 	}
 }
 
+//Create a NewSubscription. The returned subscription must be unsubscribed on clean up
 func (p *PubSub) NewSubscription(channelNames []string) Subscription {
 	pipeChannel := make(chan Message)
 
@@ -70,34 +75,35 @@ func (p *PubSub) NewSubscription(channelNames []string) Subscription {
 	for _, channelName := range channelNames {
 		channel := p.getChannel(channelName)
 
-		go func () {
+		go func() {
 			for {
 				select {
-					case value := <- channel:
-						go func () {
-							pipeChannel <- value
-						}()
-						return
-					case <- ctx.Done():
-						return
-					case <- p.context.Done():
-						return 
+				case value := <-channel:
+					go func() {
+						pipeChannel <- value
+					}()
+					return
+				case <-ctx.Done():
+					return
+				case <-p.context.Done():
+					return
 				}
 			}
 		}()
 	}
 
-	return Subscription {
+	return Subscription{
 		pipeChannel: pipeChannel,
-		cancel: cancel,
+		cancel:      cancel,
 	}
 }
 
-
-func  (subscription *Subscription) Channel() <-chan Message {
+//The channel returned will recieve data from the published data to the subscribed channels
+func (subscription *Subscription) Channel() <-chan Message {
 	return subscription.pipeChannel
 }
 
+//Unsubscribe to the subscribed channels
 func (subscription *Subscription) UnSubscribe() {
 	subscription.cancel()
 }
--- pubsub_test.go
+++ pubsub_test.go
@@ -8,11 +8,10 @@ import (
 	"time"
 )
 
-type Func func (message pubsub.Message, expectedChannelName string, expectedValue interface{})
-
+type Func func(message pubsub.Message, expectedChannelName string, expectedValue interface{})
 
 func getAsserter(t *testing.T) Func {
-	return func (message pubsub.Message, expectedChannelName string, expectedValue interface{}) {
+	return func(message pubsub.Message, expectedChannelName string, expectedValue interface{}) {
 		if message.ChannelName != expectedChannelName {
 			log.Fatalf("Expected %s channel, got %s \n", expectedChannelName, message.ChannelName)
 		}
@@ -23,18 +22,17 @@ func getAsserter(t *testing.T) Func {
 	}
 }
 
-
 func TestPubSub(t *testing.T) {
 	assert := getAsserter(t)
 
-	t.Run("Should be able to publish and subscribe to channels and should be able to receive published values", func (t *testing.T) {
+	t.Run("Should be able to publish and subscribe to channels and should be able to receive published values", func(t *testing.T) {
 		ctx := context.Background()
-		
+
 		p := pubsub.New(ctx)
 
-		sub := p.NewSubscription([] string { "one", "two", "three" })
+		sub := p.NewSubscription([]string{"one", "two", "three"})
 
-		p.Publish([]string {"one"}, 100)
+		p.Publish([]string{"one"}, 100)
 
 		ch := sub.Channel()
 
@@ -42,26 +40,26 @@ func TestPubSub(t *testing.T) {
 
 		assert(message, "one", 100)
 
-		p.Publish([] string {"two"}, 201)
+		p.Publish([]string{"two"}, 201)
 
-		message = <- ch
+		message = <-ch
 
 		assert(message, "two", 201)
 
-		p.Publish([] string {"four"}, 300)
+		p.Publish([]string{"four"}, 300)
 
-		p.Publish([] string {"three"}, 400)
+		p.Publish([]string{"three"}, 400)
 
-		message = <- ch
+		message = <-ch
 
 		assert(message, "three", 400)
 	})
 
-	t.Run("should not receive published changes after un subscribe", func (t *testing.T){
+	t.Run("should not receive published changes after un subscribe", func(t *testing.T) {
 		ctx := context.Background()
 		p := pubsub.New(ctx)
 
-		sub := p.NewSubscription([] string {"one", "two","three", "four"})
+		sub := p.NewSubscription([]string{"one", "two", "three", "four"})
 
 		ch := sub.Channel()
 
@@ -69,42 +67,41 @@ func TestPubSub(t *testing.T) {
 
 		timeout := time.After(3 * time.Second)
 
-		p.Publish([] string {"one"}, 101)
+		p.Publish([]string{"one"}, 101)
 
 		for {
 			select {
-				case <- ch:
-					log.Fatalf("Got published updates")
-				case <- timeout:
-					return
+			case <-ch:
+				log.Fatalf("Got published updates")
+			case <-timeout:
+				return
 			}
 		}
 
-	}) 
+	})
 
-	t.Run("should not receive published changes after context is canceled", func (t *testing.T){
+	t.Run("should not receive published changes after context is canceled", func(t *testing.T) {
 		ctx, cancel := context.WithCancel(context.Background())
 		p := pubsub.New(ctx)
 
-		sub := p.NewSubscription([] string {"one", "two","three", "four"})
+		sub := p.NewSubscription([]string{"one", "two", "three", "four"})
 
 		ch := sub.Channel()
 
 		timeout := time.After(3 * time.Second)
 
-		p.Publish([] string {"one"}, 101)
+		p.Publish([]string{"one"}, 101)
 
 		cancel()
 
 		for {
 			select {
-				case <- ch:
-					log.Fatalf("Got published updates after calling cancel")
-				case <- timeout:
-					return
+			case <-ch:
+				log.Fatalf("Got published updates after calling cancel")
+			case <-timeout:
+				return
 			}
 		}
 
-	}) 
+	})
 }
-
